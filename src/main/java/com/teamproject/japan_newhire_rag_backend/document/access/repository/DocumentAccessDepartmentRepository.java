9f2e67318a1c7840dbc718b08e21b994bcbd8181
kunicle <kun9626@gmail.com>
2026-08-11 14:49:04 +0900

    feat: add role and department lookup queries by access rule id

--- src/main/java/com/teamproject/japan_newhire_rag_backend/document/access/repository/DocumentAccessDepartmentRepository.java
+++ src/main/java/com/teamproject/japan_newhire_rag_backend/document/access/repository/DocumentAccessDepartmentRepository.java
@@ -1,8 +1,12 @@
 package com.teamproject.japan_newhire_rag_backend.document.access.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.teamproject.japan_newhire_rag_backend.document.access.entity.DocumentAccessDepartment;
 
 public interface DocumentAccessDepartmentRepository extends JpaRepository<DocumentAccessDepartment, Long> {
+
+    List<DocumentAccessDepartment> findByDocumentAccessRule_DocumentAccessRuleId(Long documentAccessRuleId);
 }
